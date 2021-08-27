8f6c700c4bf23697e033d2567ba6f53087c7ebec
hungnguyen.130420@gmail.com <hungnguyen.130420@gmail.com>
2021-08-27 18:44:32 +0700

    edited proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,26 +4,25 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import {setContext} from 'apollo-link-context';
+import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://cryptic-peak-14681.herokuapp.com/",
 });
 
-const authLink = setContext(()=>{
-  const token = localStorage.getItem('jwtToken');
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
   return {
-    headers:{
-      Authorization: token ? `Bearer ${token}` : ''
-    }
-  }
-})
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link:authLink.concat(httpLink),       
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 
 export default (
   <ApolloProvider client={client}>
